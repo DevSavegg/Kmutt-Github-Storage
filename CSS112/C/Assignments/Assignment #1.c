860ea195973e4791c7a99b893410e93927543c1b
DevSavegg <contact.devsavegg@gmail.com>
2024-08-22 23:44:15 +0700

    Added Assignment Directory (CSS112)

---
+++ CSS112/C/Assignments/Assignment #1.c
@@ -0,0 +1,17 @@
+// Find LCM and GCD of any length of numbers
+#include <stdio.h>
+
+int findLCM(int* numList, int length) {
+
+}
+
+int findGCD(int* numList, int length) {
+
+}
+
+int main() {
+
+    
+
+    return 0;
+}
---
+++ CSS112/C/Assignments/Assignment #2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+
+Objective: Print pyramid algorithm of the following pattern:
+
+********
+ *+++++*
+  *++++*
+   *+++*
+    *++*
+     *+*
+      **
+       *
+
+*/
+
+int main() {
+    int numberOfRow;
+
+    printf("Enter the number of row: ");
+    scanf("%i", &numberOfRow);
+
+    for (int i = 0; i < numberOfRow; i++) {
+        for (int j = 0; j < numberOfRow; j++) {
+            
+        }
+    }
+
+    return 0;
+}
