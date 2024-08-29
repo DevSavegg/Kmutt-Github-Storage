50faae7765e50c61812afc78e06256dc40a4a451
DevSavegg <contact.devsavegg@gmail.com>
2024-08-29 22:23:22 +0700

    Finished all assignments (CSS112)

--- CSS112/C/Assignments/Assignment #4.c
+++ CSS112/C/Assignments/Assignment #4.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 
-int** binary_convert(int number) {
-    int binary[][8] = {0};
-    int count = 0;
-    while (number != 0) {
-        for (int i = 7; i >= 0; i++) {
-            binary[count][i] = number % 2;
+void binary_convert(int binaryArray[][8], int number, int rows) {
+    for (int count = rows - 1; count >= 0; count--) {
+        for (int i = 7; i >= 0; i--) {
+            binaryArray[count][i] = number % 2;
             number = number / 2;
         }
-        count++;
     }
-
-    return binary;
 }
 
 int main() {
-    int inputNumber, row, column, i, j;
-    int** binary_result;
+    int inputNumber, i, j;
+    int row, column = 8;
 
     printf("Input the number: ");
     scanf("%d", &inputNumber);
 
-    binary_result = binary_convert(inputNumber);
+    row = (inputNumber == 0) ? 1 : (sizeof(int) * 8 - __builtin_clz(inputNumber) + 7) / 8;
 
-    row = sizeof(binary_result) / sizeof(binary_result[0]);
-    column = sizeof(binary_result[0])/row;
+    int binary_result[row][column];
 
-    printf("Result: ");
+    binary_convert(binary_result, inputNumber, row);
+
+    printf("Result:\n");
     for (i = 0; i < row; i++) {
-        for (j = 0; i < column; i++) {
+        for (j = 0; j < column; j++) {
             printf("%d", binary_result[i][j]);
         }
+        printf("\n");
     }
 
     return 0;
